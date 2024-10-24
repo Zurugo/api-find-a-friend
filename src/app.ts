96dd7c690306ceb7123f826313d8275262963fae
HugoSalvador <hugossalvador1241@gmail.com>
2024-10-24 20:26:51 -0300

    Create a configuration for JWT in app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+
 import { env } from './.env'
 import { ZodError } from 'zod'
-
 
 import { usersRoutes } from './http/controllers/users/routes'
 
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'RefreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m',
+    },
+})
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 
---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(app: FastifyInstance, isOrg = false) {
+    await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+            role: isOrg ? 'ORG' : 'MEMBER'
+        }
+    })
+
+    // await request(app.server).post('/users').send({
+    //     name: 'John'
+    // })
+}
